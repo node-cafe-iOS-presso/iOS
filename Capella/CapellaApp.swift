2213ffdc41ef36229f48be25ae61d77cd1fb426d
FpRaArNkK <mspark1184@naver.com>
2024-02-17 07:05:16 +0900

    Feat: Firebase 작업 완료

--- Capella/CapellaApp.swift
+++ Capella/CapellaApp.swift
@@ -7,8 +7,22 @@
 
 import SwiftUI
 
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 @main
 struct CapellaApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
