258a43ec12ee52e4fa0165017ecca7b46a51b1b1
youz2me <kynhun20@gachon.ac.kr>
2024-02-16 22:10:00 +0900

    Build: Initial Commit

---
+++ Capella/CapellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapellaApp.swift
+//  Capella
+//
+//  Created by YOUJIM on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CapellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
