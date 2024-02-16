f7defa3c01f7a9cf59d317f4c169b7b1a3012733
youz2me <kynhun20@gachon.ac.kr>
2024-02-17 08:47:59 +0900

    Feat: 온보딩 API model 생성

--- Capella/Service/APIConstant.swift
+++ Capella/Service/APIConstant.swift
@@ -10,50 +10,14 @@ import Foundation
 
 
 struct Constant {
-    static let baseURL = "baseURl"
+    static let baseURL = "https://api-dev-v2.mappilogue.shop"
     
-    static let postAuthEmailCheck = "/auth/email-check"
-    static let getAuthFriends = "/auth/friends"
-    static let getSearchUser = "/user/search"
-    static let postAuthLogin = "/auth/login"
-    static let getPlaces = "/places"
-    static let getPlacesSearch = "/places/search"
-    static let postRecord = "/records"
-    static func getPlaceId(placeId: Int) -> String {
-        return "/places/\(placeId)"
+    static let createUser = "/user"
+    static func getFindUser(token: String) -> String {
+        return "/user/:\(token)"
     }
-    static func getPlaceNewsURL(placeId: Int) -> String {
-        return "\(getPlaceId(placeId: placeId))/news"
+    static func patchUpdateProfile(id: String) -> String {
+        return "/user/:\(id)"
     }
-    static func getPlaceBooksURL(placeId: Int) -> String {
-        return "\(getPlaceId(placeId: placeId))/books"
-    }
-    static func getPlaceActivitiesURL(placeId: Int) -> String {
-        return "\(getPlaceId(placeId: placeId))/activities"
-    }
-    static func getPlaceReviewsURL(placeId: Int) -> String {
-        return "\(getPlaceId(placeId: placeId))/reviews"
-    }
-    static let getPlaceId = "/places/:placeId"
-    static func postPlacesReviews(placeId: Int) -> String {
-        return "/places/\(placeId)/reviews"
-    }
-    static func postReviewsImages(reviewId: Int) -> String {
-        return "/reviews/\(reviewId)/images"
-    }
-    static func getBooksList(title: String) -> String {
-        return "/books/list?title=\(title)"
-    }
-    static func postRecordsImages(recordId: Int) -> String {
-        return "/records/\(recordId)/images"
-    }
-    static let getUsersOutline = "/users/outline"
-    static let getUsersActivities = "/users/activities"
-    static let getUsersReviews = "/users/reviews"
-    static func getUsersRecords(category: Int) -> String {
-        return "/users/records?category=\(category)"
-    }
-    
-    static let getRecordsFriends = "/records/friends"
 }
 
