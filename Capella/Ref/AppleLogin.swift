d1281db759c747a3d522eb01482668af4d8a768f
FpRaArNkK <mspark1184@naver.com>
2024-02-16 23:09:36 +0900

    Feat: SwiftUI native 코드 추가

--- Capella/Ref/AppleLogin.swift
+++ Capella/Ref/AppleLogin.swift
@@ -22,7 +22,7 @@ struct SignInWithAppleButtonView: UIViewRepresentable {
     }
 }
 
-struct LoginView: View {
+struct LoginTestView_UIKit: View {
     var body: some View {
         SignInWithAppleButtonView()
             .frame(width: 280, height: 60, alignment: .center)
@@ -36,5 +36,30 @@ struct LoginView: View {
 
         let controller = ASAuthorizationController(authorizationRequests: [request])
         controller.performRequests()
+    }
+}
+
+// MARK : ----- 또는 -----
+
+struct LoginTestView_SwiftUI: View {
+    var body: some View {
+        SignInWithAppleButton(
+            onRequest: { request in
+                // 애플 로그인 요청 설정
+            },
+            onCompletion: { result in
+                // 애플 로그인 완료 후 처리
+                switch result {
+                case .success(let authResults):
+                    // 로그인 성공
+                    print("Authentication successful: \(authResults)")
+                case .failure(let error):
+                    // 로그인 실패
+                    print("Error occurred: \(error.localizedDescription)")
+                }
+            }
+        )
+        .frame(width: 280, height: 60)
+        .cornerRadius(12)
     }
 }
